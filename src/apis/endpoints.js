ecd6065c3e3ede99c754b1f7f069cdceaa5de285
James Manuel <moodyjmz@users.noreply.github.com>
2025-04-07 00:58:48 +0200

    chore: :wrench: Upgrade to use vite

--- src/apis/endpoints.js
+++ src/apis/endpoints.js
@@ -1,5 +1,5 @@
 // Map from framework specific env vars to JS consts for services
-export const COUNTRIES_SERVICE_URL = process.env.REACT_APP_COUNTRIES_SERVICE_URL;
-//export const ISS_CURRENT_URL = process.env.REACT_APP_ISS_CURRENT_URL;
+export const COUNTRIES_SERVICE_URL = import.meta.env.VITE_COUNTRIES_SERVICE_URL;
+//export const ISS_CURRENT_URL = import.meta.env.VITE_ISS_CURRENT_URL;
 export const ISS_CURRENT_URL = 'http://api.open-notify.org/iss-now.json';
-export const ISS_FUTURE_URL = process.env.REACT_APP_ISS_PASSES_URL;
+export const ISS_FUTURE_URL = import.meta.env.VITE_ISS_PASSES_URL;
--- src/feature/map/services/CountriesService.js
+++ src/feature/map/services/CountriesService.js
@@ -3,5 +3,6 @@ import { sortCities } from '../utils/cities';
 import { fetchWithRetries } from './BaseService';
 
 export function fetchCountries(transport = {}, retries = 3) {
+  console.log('COUNTRIES_SERVICE_URL', COUNTRIES_SERVICE_URL);
   return fetchWithRetries({url:COUNTRIES_SERVICE_URL, callback: sortCities, transport}, retries);
 }
