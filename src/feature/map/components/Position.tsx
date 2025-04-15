e376271eef649fe67f5f1f7eed3d4fa280089e97
James Manuel <moodyjmz@users.noreply.github.com>
2025-04-15 10:20:37 +0200

    chore: Add types to position component

--- src/feature/map/components/Position.tsx
+++ src/feature/map/components/Position.tsx
@@ -1,7 +1,15 @@
-export default function Position({ position }) {
+import { JSX } from 'react';
+import { Coordinates } from '../defs/coordinates';
+
+// Define a type for the position prop
+interface PositionProps {
+    position: Coordinates | null;
+}
+
+export default function Position({ position }: PositionProps): JSX.Element {
     return (
         <>
             {position ? `${position.latitude}, ${position.longitude}` : null}
         </>
-    )
+    );
 }
