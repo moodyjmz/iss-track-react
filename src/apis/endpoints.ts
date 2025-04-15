38ead742469988a259e42c7843051cc95387354a
James Manuel <moodyjmz@users.noreply.github.com>
2025-04-15 12:08:32 +0200

    chore: :wrench: Change to local countries

--- src/apis/endpoints.ts
+++ src/apis/endpoints.ts
@@ -1,6 +1,5 @@
 // Map from framework-specific environment variables to TypeScript constants for services
 
 export const COUNTRIES_SERVICE_URL: string = import.meta.env.VITE_COUNTRIES_SERVICE_URL as string;
-// export const ISS_CURRENT_URL: string = import.meta.env.VITE_ISS_CURRENT_URL as string;
-export const ISS_CURRENT_URL: string = 'http://api.open-notify.org/iss-now.json';
+export const ISS_CURRENT_URL: string = import.meta.env.VITE_ISS_CURRENT_URL as string;
 export const ISS_FUTURE_URL: string = import.meta.env.VITE_ISS_PASSES_URL as string;
